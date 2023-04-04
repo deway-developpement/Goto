def4222b477350d9760390d73f96c3865ec95d1c
AxelLns <axel.loones@gmail.com>
2023-04-04 18:15:33 +0200

    feat(Map, TabBarButton): implemetation of gpx lines, progression, +fix on tabBarButton while loading

--- app/src/components/Map/Map.jsx
+++ app/src/components/Map/Map.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
-import MapView, { Marker, UrlTile } from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import { Text, View, ActivityIndicator } from 'react-native';
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import * as Location from 'expo-location';
+import stylesheet from './style';
+import { useTheme } from '@react-navigation/native';
+import CONTENTGPX from './gpx';
+import { DOMParser } from 'xmldom';
 
-export default function Map({ location }) {
+function Map({ location }) {
+    const { colors } = useTheme();
+    let [leftPoints, setLeftPoints] = useState([]);
+    let [passedPoints, setPassedPoints] = useState([]);
+
+    function parseFile() {
+        const doc = new DOMParser().parseFromString(CONTENTGPX, 'text/xml');
+        const trkpts = doc.getElementsByTagName('trkpt');
+        const trkptsArray = Array.from(trkpts);
+        const trkptsArrayLat = trkptsArray.map((trkpt) =>
+            trkpt.getAttribute('lat')
+        );
+        const trkptsArrayLon = trkptsArray.map((trkpt) =>
+            trkpt.getAttribute('lon')
+        );
+        return trkptsArrayLat.map((lat, index) => {
+            return {
+                latitude: parseFloat(lat),
+                longitude: parseFloat(trkptsArrayLon[index]),
+            };
+        });
+    }
+
+    useEffect(() => {
+        setLeftPoints(parseFile());
+    }, []);
+
+    function advance() {
+        if (leftPoints.length == 0) {
+            return;
+        }
+        const gpxPathLeft = leftPoints.slice();
+        const gpxPathpassedPoints = passedPoints.slice();
+        gpxPathpassedPoints.push(gpxPathLeft.shift());
+        gpxPathpassedPoints.push(gpxPathLeft[0]);
+        setPassedPoints(gpxPathpassedPoints);
+        setLeftPoints(gpxPathLeft);
+    }
+
     return (
         <MapView
             initialRegion={{
@@ -20,21 +64,97 @@ export default function Map({ location }) {
             style={{ flex: 1, width: '100%' }}
             maxZoomLevel={17}
         >
-            <UrlTile
-                urlTemplate="http://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
-                maximumZ={19}
-                // tileCachePath={
-                //     Platform.OS === 'android' ? '/assets/maps' : 'assets/maps'
-                // }
-                // tileMaxCacheSize={100000}
-                shouldReplaceMapContent={true}
+            <Polyline
+                coordinates={passedPoints}
+                strokeColor={colors.borderlinesecondary}
+                strokeWidth={6}
+            />
+            <Polyline
+                coordinates={passedPoints}
+                strokeColor={colors.linesecondary}
+                strokeWidth={4}
+            />
+            <Polyline
+                coordinates={leftPoints}
+                strokeColor={colors.borderlineprimary}
+                strokeWidth={6}
+            />
+            <Polyline
+                coordinates={leftPoints}
+                strokeColor={colors.lineprimary}
+                strokeWidth={4}
             />
             <Marker
                 coordinate={{
                     latitude: parseFloat(location?.coords?.latitude),
                     longitude: parseFloat(location?.coords?.longitude),
                 }}
+                onPress={() => {
+                    advance();
+                }}
             />
         </MapView>
+    );
+}
+
+export default function MapScreen() {
+    const { colors } = useTheme();
+    const styles = stylesheet(colors);
+
+    const [permission, request] = Location.useForegroundPermissions();
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        if (permission === null) {
+            request();
+        } else if (
+            permission.granted === false &&
+            permission.canAskAgain === false
+        ) {
+            setErrorMsg('Permission to access location was denied');
+        } else if (
+            permission.granted === false &&
+            permission.canAskAgain === true
+        ) {
+            request();
+        } else if (permission.granted === true) {
+            Location.getLastKnownPositionAsync({}).then((response) => {
+                setLocation(response);
+            });
+            Location.watchPositionAsync({}, (response) => {
+                setLocation(response);
+            });
+        }
+    }, [permission]);
+
+    useEffect(() => {
+        console.log(location?.coords?.latitude);
+    }, [location]);
+
+    return (
+        <View style={styles.container}>
+            {(() => {
+                if (location == null) {
+                    if (errorMsg != null) {
+                        return (
+                            <Text style={{ alignSelf: 'center' }}>
+                                {errorMsg}
+                            </Text>
+                        );
+                    } else {
+                        return (
+                            <ActivityIndicator
+                                size="large"
+                                color="#0000ff"
+                                style={{ flex: 3, width: '100%' }}
+                            />
+                        );
+                    }
+                } else {
+                    return <Map location={location} />;
+                }
+            })()}
+        </View>
     );
 }
