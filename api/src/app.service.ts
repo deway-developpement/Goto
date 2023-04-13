eb845f5b61b2c6cb1b69299afff70532601ad6c6
P4UL-M <paul.mairesse@free.fr>
2023-04-13 11:21:37 +0200

    feat(App): Security check for file reading

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, StreamableFile } from '@nestjs/common';
+import { Injectable, StreamableFile, UnauthorizedException } from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { join } from 'path';
 
@@ -9,6 +9,10 @@ export class AppService {
     }
 
     getFileStream(category: string, id: string): StreamableFile {
+        // check that the id is safe and the user isn't trying to access a file outside of the category
+        if (!id.match(/^[0-9a-fA-F\-]{36}$/)) {
+            throw new UnauthorizedException();
+        }
         // pipe file from local storage
         const file = createReadStream(join(process.cwd(), `./data/${category}/${id}`));
         return new StreamableFile(file);
