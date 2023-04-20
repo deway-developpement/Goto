274d143c2b273ba426b5adff31fc7260c81d5a69
P4UL-M <paul.mairesse@free.fr>
2023-04-20 08:13:35 +0200

    feat(ApolloProvider,alert,category,hike,performance,poi,tag): File upload + AuthType Enum

--- app/src/providers/ApolloProvider.jsx
+++ app/src/providers/ApolloProvider.jsx
@@ -2,7 +2,6 @@ import {
     ApolloClient,
     InMemoryCache,
     from,
-    HttpLink,
     ApolloProvider,
 } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
@@ -13,6 +12,7 @@ import React, { useContext } from 'react';
 import { refreshAuth } from '../services/auth.service';
 import { default as config } from '../config.json';
 import apolloLogger from 'apollo-link-logger';
+import { createUploadLink } from 'apollo-upload-client';
 
 function createApolloClient(authContext) {
     const authLink = setContext(() => {
@@ -27,62 +27,64 @@ function createApolloClient(authContext) {
     let pendingRequests = [];
 
     // @ts-ignore
-    const refreshLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
-        if (graphQLErrors) {
-            for (let err of graphQLErrors) {
-                switch (err.extensions.code) {
-                case 'UNAUTHENTICATED': {
-                    // Here we create a new Promise that will be resolved when the token refresh is complete
-                    let _forward;
-                    if (!isRefreshing) {
-                        isRefreshing = true;
-                        _forward = fromPromise(
-                            refreshAuth(authContext)
-                                .then((token) => {
-                                    // Once the token refresh is complete, we update the pending requests with the new token
-                                    operation.setContext({
-                                        headers: {
-                                            Authorization: `Bearer ${token}`,
-                                        },
-                                    });
-                                    return token;
-                                })
-                                .catch(() => {
-                                    // If the token refresh fails, we remove the pending requests and log the user out
-                                    pendingRequests = [];
-                                    authContext.logout();
-                                    return;
-                                })
-                                .finally(() => {
-                                    // Finally, we send the requests that were pending and reset the isRefreshing flag
-                                    isRefreshing = false;
-                                })
-                        ).filter(value => Boolean(value));
-                    } else {
-                        // Will only emit once the Promise is resolved
-                        _forward = fromPromise(
-                            new Promise(resolve => {
-                                pendingRequests.push(() => resolve());
-                            }) // this line create a Promise and add its resolve function to the pendingRequests array
-                        );
+    const refreshLink = onError(
+        ({ graphQLErrors, networkError, operation, forward }) => {
+            if (graphQLErrors) {
+                for (let err of graphQLErrors) {
+                    switch (err.extensions.code) {
+                    case 'UNAUTHENTICATED': {
+                        // Here we create a new Promise that will be resolved when the token refresh is complete
+                        let _forward;
+                        if (!isRefreshing) {
+                            isRefreshing = true;
+                            _forward = fromPromise(
+                                refreshAuth(authContext)
+                                    .then((token) => {
+                                        // Once the token refresh is complete, we update the pending requests with the new token
+                                        operation.setContext({
+                                            headers: {
+                                                Authorization: `Bearer ${token}`,
+                                            },
+                                        });
+                                        return token;
+                                    })
+                                    .catch(() => {
+                                        // If the token refresh fails, we remove the pending requests and log the user out
+                                        pendingRequests = [];
+                                        authContext.logout();
+                                        return;
+                                    })
+                                    .finally(() => {
+                                        // Finally, we send the requests that were pending and reset the isRefreshing flag
+                                        isRefreshing = false;
+                                    })
+                            ).filter((value) => Boolean(value));
+                        } else {
+                            // Will only emit once the Promise is resolved
+                            _forward = fromPromise(
+                                new Promise((resolve) => {
+                                    pendingRequests.push(() => resolve());
+                                }) // this line create a Promise and add its resolve function to the pendingRequests array
+                            );
+                        }
+                        // We return the Observable that will be fetched once the token refresh is complete
+                        return _forward.flatMap(() => forward(operation));
                     }
-                    // We return the Observable that will be fetched once the token refresh is complete
-                    return _forward.flatMap(() => forward(operation));
+                    }
                 }
-                }  
+            }
+            if (networkError) {
+                console.log(`[Network error]: ${networkError}`);
             }
         }
-        if (networkError) {
-            console.log(`[Network error]: ${networkError}`);
-        }
-    });
-    
-    const httpLink = new HttpLink({
+    );
+
+    const httpLink = new createUploadLink({
         uri: config.graphQLLink,
     });
 
     const client = new ApolloClient({
-        link: from([ apolloLogger, authLink, refreshLink, httpLink ]),
+        link: from([apolloLogger, authLink, refreshLink, httpLink]),
         cache: new InMemoryCache(),
     });
 
@@ -92,9 +94,7 @@ function createApolloClient(authContext) {
 const AuthApolloProvider = ({ children }) => {
     const authContext = useContext(AuthContext);
     const client = createApolloClient(authContext);
-    return <ApolloProvider client={client}>
-        {children}
-    </ApolloProvider>;
+    return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
 export default AuthApolloProvider;
