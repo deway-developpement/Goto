6ab0544db99c932df9a46a3e13c08700b9878309
P4UL-M <paul.mairesse@free.fr>
2023-04-20 16:17:37 +0200

    fix(photo): missing permissions

--- api/src/photo/photo.resolver.ts
+++ api/src/photo/photo.resolver.ts
@@ -5,8 +5,9 @@ import { CurrentUser, GqlAuthGuard } from '../auth/guards/graphql-auth.guard';
 import { PhotoDTO } from './interfaces/photo.dto';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as _ from '@nestjs-query/query-graphql/node_modules/@nestjs-query/core';
-import { PhotoInput } from './interfaces/photo.input';
+import { ObjType, PhotoInput } from './interfaces/photo.input';
 import { UserDTO } from '../user/interfaces/user.dto';
+import { AuthType } from '../auth/interface/auth.type';
 
 @Resolver(() => PhotoDTO)
 export class PhotoResolver {
@@ -18,7 +19,10 @@ export class PhotoResolver {
         @Args('input') query: PhotoInput,
         @CurrentUser() user: UserDTO
     ): Promise<PhotoDTO> {
-        if (user.credidential < 2) {
+        if (
+            user.credidential < AuthType.superAdmin ||
+            !(query.objType === ObjType.USER || query.objId === user.id) // if not superAdmin, only allow to create photo for himself
+        ) {
             throw new UnauthorizedException();
         }
         return this.service.create(query);
