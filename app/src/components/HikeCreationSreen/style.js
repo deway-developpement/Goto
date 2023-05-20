00d19927b97c20d7c41f24a120dae13db93fca17
Looki-fr <lemeilllouis@gmail.com>
2023-05-20 15:28:42 +0200

    build(HikeCreation): style

--- app/src/components/HikeCreationSreen/style.js
+++ app/src/components/HikeCreationSreen/style.js
@@ -2,13 +2,6 @@ import { StyleSheet } from 'react-native';
 
 const stylesheet = (colors) =>
     StyleSheet.create({
-        blur: {
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-        },
         containerLogin: {
             flex: 1,
             marginTop: '28%',
@@ -82,6 +75,7 @@ const stylesheet = (colors) =>
             backgroundColor: colors.link,
             borderRadius: 6,
             padding: 10,
+            alignSelf:'center'
         },
         btnText: {
             fontWeight: '600',
